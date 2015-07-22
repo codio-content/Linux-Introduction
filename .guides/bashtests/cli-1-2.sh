ab13bc235fc140bdcd88f83cad6436c22c2a6c55
netpoe <yo@soygus.com>
2015-07-22 19:06:26 +0000

    Challenge Sec. 13 - Updated system

---
+++ .guides/bashtests/cli-1-2.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# bash_history edit
+
+bash_history=~/.bash_history
+check_file=cli-1-2.txt
+grep -A2000 -e "^cli-1-2" $bash_history > "$BASHDIR/bashtests/$check_file"
+
+check_file="$BASHDIR/bashtests/$check_file"
+
+QCOUNT=2
+function expect_command 
+{
+	(( QCOUNT -- ))
+	if grep -Fxq "$1" "$check_file"
+	then
+		response "$1" "$3" $QCOUNT
+	else 
+		tell_error "$1" "$2" $QCOUNT
+	fi
+}
+
+function tell_error 
+{
+	echo "You are missing task $(( $3 + 1 )) command right. Expected: $2. Try again."
+	return 1
+	exit
+}
+
+function response 
+{
+	echo "$(($3+1)). The '$1' command $2"
+	test_command
+}
+
+function test_command {
+	while [[ $QCOUNT > 0 ]]; do
+		case $QCOUNT in
+			1 )
+				expect_command "ls" "list files and directories" "lists files and directories"
+				;;
+			2 )
+				expect_command "ls -al" "list hidden files and directories" "together with the '-a' option lists hidden files and directories"
+				;;
+		esac
+	done
+}
+
+
+test_command
--- .guides/content/Challenge--The-list-command-bda7.md
+++ .guides/content/Challenge--The-list-command-bda7.md
@@ -3,9 +3,9 @@
 |||challenge
 By listing files and directories you can compare the output with your file tree.
 
-Click on the command line at the left and execute:
+Click on the command line at the left and:
 
-```
-bashtest cli-2-test
-```
+1. List the files and directories that are on the `~/workspace` parent directory
+2. Lets make sure we have dotfiles available by listing the hidden files
+
 |||
