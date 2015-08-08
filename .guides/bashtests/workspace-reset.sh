2c0d47369ddf3dc4f39eac1d2e4ee6bdf8b9ddd0
netpoe <yo@soygus.com>
2015-08-08 21:31:41 +0000

    Workspace-reset command

---
+++ .guides/bashtests/workspace-reset.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# workspace-reset file hierarchy
+
+WORKSPACE=/home/codio/workspace/
+arg=$1
+
+function init_workspace {
+	mkdir "${WORKSPACE}dir1"
+	touch "${WORKSPACE}dir1/file1.txt" "${WORKSPACE}empty-file.txt"
+	echo "Project files have been reseted"
+	sleep 2
+	clear
+}
+
+function reset_workspace {
+	rm -r "${WORKSPACE}dir1"
+	rm -r "${WORKSPACE}empty-file.txt"
+	echo "Reseting project files and directories"
+	sleep 2
+	init_workspace
+}
+
+function reset_challenges {
+	find "${WORKSPACE}.guides/bashtests/" -path "*.txt" -delete
+	# find "${WORKSPACE}/bashtests/" -path "*.txt" -delete
+	echo "Project challenges files have been reseted."
+	sleep 2
+	clear
+}
+
+case $arg in
+	challenges ) reset_challenges
+		;;
+	workspace ) reset_workspace
+		;;
+	* ) echo "'$arg' doesn't match any available arguments" 
+		;;
+esac
